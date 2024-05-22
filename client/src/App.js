5dd8ddffd8667fcf7d37bc959006e88d6571611e
sshaw00 <swarupk102001@gmail.com>
2024-05-22 15:21:54 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,44 @@
+import {
+  BrowserRouter,
+  Navigate,
+  Routes,
+  Route,
+  Outlet,
+} from "react-router-dom";
+import Dashboard from "./pages/dashboard";
+import Home from "./pages/Home";
+
+import { useSelector } from "react-redux";
+import { SnackbarProvider } from "notistack";
+
+// const PrivateRoutes = () => {
+//   const { isAuth } = useSelector((state) => state.auth);
+
+//   return <>{isAuth ? <Outlet /> : <Navigate to="/login" />}</>;
+// };
+
+// const RestrictedRoutes = () => {
+//   const { isAuth } = useSelector((state) => state.auth);
+
+//   return <>{!isAuth ? <Outlet /> : <Navigate to="/dashboard" />}</>;
+// };
+
+const App = () => {
+  return (
+    <SnackbarProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          <Route>
+            {/* <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} /> */}
+            <Route path="/" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </SnackbarProvider>
+  );
+};
+
+export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,8 @@
+import Layout from "../components/layout";
+import "./login.css";
+
+const Home = () => {
+  return <Layout></Layout>;
+};
+
+export default Home;
